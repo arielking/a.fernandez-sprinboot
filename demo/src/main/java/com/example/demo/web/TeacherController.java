49a8262386e004c4a697313ba99ef80bab5cc334
Ariel Fernandez <arielunix@gmail.com>
2018-04-26 21:22:51 -0400

    Add files via upload

---
+++ demo/src/main/java/com/example/demo/web/TeacherController.java
@@ -0,0 +1,26 @@
+package com.example.demo.web;
+
+import com.example.demo.domain.Teacher;
+import com.example.demo.service.TeacherService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * Created by jhonny on 5/18/17.
+ */
+@RestController
+@RequestMapping("/teachers")
+public class TeacherController {
+
+    @Autowired
+    TeacherService teacherService;
+    @RequestMapping(method = RequestMethod.GET)
+    public List<Teacher> getTeacher(){
+        return teacherService.getAllTeachers();
+    }
+}
+
